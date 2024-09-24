dd5f8b001ede20116e5c6993b21217d2255d060f
lyes9 <ilyes.hamza.debbab@gmail.com>
2024-09-24 14:01:18 +0200

    lunch

---
+++ src/main/java/appli/todolistjx/HelloApplication.java
@@ -0,0 +1,23 @@
+package appli.todolistjx;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        stage.setTitle("PURPULE APP");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
